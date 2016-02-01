01bb6b62cbb311857acdc26e31a92caf0009b42a
nnoldman <1527053436@qq.com>
2016-02-01 21:34:29 +0800

    娣诲姞level

---
+++ data/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager 
+{
+    public static void LoadLevel(int levelID)
+    {
+
+    }
+    public static void LoadMap(int mapID)
+    {
+
+    }
+    public static void LoadTerrain(int terrainID)
+    {
+
+    }
+}
---
+++ data/NpcCreator.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SpawnPoint
+{
+    public int x;
+    public int y;
+    public int npcid;
+    public int npcCount;
+    public int intervel;
+    public int nextTime;
+}
+public class NpcCreator : MonoBehaviour
+{
+    List<SpawnPoint> mPointList;
+    int mBossNpcID;
+
+    Clock mBornTimer;
+    Clock mPauseTimer;
+
+    SpawnPoint mCurPoint;
+
+    int mTrun = 0;
+
+    void OnPauseTimerEnd(Clock c)
+    {
+        if (NextPoint())
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    void OnBornTimerEnd(Clock c)
+    {
+        mPauseTimer.Begin(mCurPoint.nextTime * 0.001f, OnPauseTimerEnd, true);
+    }
+    void OnBornTimer(Clock c)
+    {
+    }
+    void SetBornInfo()
+    {
+        mBornTimer.Begin(mCurPoint.intervel * 0.001f, OnBornTimerEnd, true);
+    }
+    public bool NextPoint()
+    {
+        if (mTrun < mPointList.Count)
+        {
+            mCurPoint = mPointList[mTrun];
+            mTrun++;
+            SetBornInfo();
+            return true;
+        }
+        return false;
+    }
+    void Start()
+    {
+        mBornTimer = ClockMgr.Instance.Require();
+        mBornTimer.OnTimer = OnBornTimer;
+    }
+    void Update()
+    {
+
+    }
+}
