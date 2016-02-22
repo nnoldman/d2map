81400f8f31e0f62091f89785a5c66246c7b665fe
nnoldman <1527053436@qq.com>
2016-02-22 20:27:57 +0800

    2d aniamtion

---
+++ 2DAnimation/Animation2D.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation2D : MonoBehaviour
+{
+    public ATextureAnimation anim;
+    
+    public int animationDataID = 0;
+
+    GameData.Direction dir
+    {
+        get
+        {
+            float rotation = transform.rotation.eulerAngles.z;
+            return (GameData.Direction)((int)rotation / 30);
+        }
+        set
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 30 * (int)value);
+            Play();
+        }
+    }
+
+
+    public void Play()
+    {
+        GameData.AnimationData data = GameData.AnimationData.Get(animationDataID);
+        
+        if (data)
+        {
+            GameData.AnimationInfo2D info = data.GetAnimationByDir(dir);
+            
+            if (info != null && anim)
+            {
+                UITexture uitexture = GetComponent<UITexture>();
+                if (uitexture)
+                {
+                    uitexture.mainTexture = Gen.LoadResource<Texture>(info.path);
+                    anim.baseX = info.x;
+                    anim.baseY = info.y;
+                    anim.frameWidth = info.width;
+                    anim.frameHeight = info.height;
+                    anim.frameCount = info.count;
+                    anim.frameCountPerLine = info.countPerLine;
+                    anim.Begin();
+                }
+            }
+        }
+    }
+}
---
+++ 2DAnimation/AnimationData.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace GameData
+{
+    public enum Direction
+    {
+        DirRight,
+        DirRightButtom,
+        DirButtom,
+        DirLeftButtom,
+        DirLeft,
+        DirRightTop,
+        DirTop,
+        DirLeftTop,
+    }
+    public class AnimationInfo2D
+    {
+        public string path;
+        public string name;
+        public Direction dir;
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+        public int count;
+        public int countPerLine;
+    }
+    public class AnimationData : GameData<AnimationData>
+    {
+        static public readonly string fileName = "xml/AnimationData";
+
+        public List<AnimationInfo2D> animations;
+
+        public AnimationInfo2D GetAnimationByDir(Direction dir)
+        {
+            foreach (var info in animations)
+            {
+                if (info.dir == dir)
+                    return info;
+            }
+            return null;
+        }
+    }
+}
+
